0663d2732837f590e816dde03ae0703e3961085d
Bombele Liyama Camille <94522767+Bombele@users.noreply.github.com>
2025-11-27 14:03:38 -0400

    Update routers.py

--- itcaa_ai_offline/routers.py
+++ itcaa_ai_offline/routers.py
@@ -1,1 +1,13 @@
+# routes.py
+from fastapi import APIRouter
+from schemas import PredictionInput, PredictionOutput
+from predictor import predict
 
+router = APIRouter()
+
+@router.post("/predict", response_model=PredictionOutput)
+def get_prediction(input_data: PredictionInput):
+    """
+    Endpoint REST pour obtenir une prédiction IA hors ligne.
+    """
+    return predict(input_data)
